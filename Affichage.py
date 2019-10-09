a21d6406c1e494d768507c04af76272d0806ee78
Yohann THEPAUT <contact@ythepaut.com>
2019-10-09 14:25:54 +0200

    Modification temps

--- Affichage.py
+++ Affichage.py
@@ -1,6 +1,7 @@
 import os
 import pygame
 from enums.Menu import Menu
+from Utils import Utils
 
 class Affichage:
     """Classe Affichage : Gestion de l'affichage de la fenetre."""
@@ -15,15 +16,17 @@ class Affichage:
         pygame.display.set_caption("Univ Defender")
 
 
-    def rafraichirEcran(self, partie):
+    def rafraichir_ecran(self, partie):
         """Procedure qui affiche l'ensemble des elements de la fenetre (arriere plan, etudiants, profs)."""
         if self.menu == Menu.AUCUN:
-            
+
             arriere_plan = pygame.image.load(partie.carte.arriere_plan)
             self.fenetre.blit(arriere_plan, (0, 0))
-            
+
             for etudiant in partie.etudiants:
                 img_etudiant = pygame.image.load(os.path.join("resources", "img", "etudiant.png"))
                 self.fenetre.blit(img_etudiant, (etudiant.coords[0] - 20, etudiant.coords[1] - 20))
-            
+
             pygame.display.update()
+            print("update")
+            Utils.update_ticks(Utils)
