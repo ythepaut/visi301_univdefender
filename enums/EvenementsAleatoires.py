43e786acd24f7dbaf6c51b3f2ad36e3267100e27
rakotoew <55093676+rakotoew@users.noreply.github.com>
2019-11-19 11:35:40 +0100

    Ajout evenements speciaux

---
+++ enums/EvenementsAleatoires.py
@@ -0,0 +1,10 @@
+"""Module Enum:EvenementsAleatoires"""
+
+from enum import Enum
+
+class EvenementsAleatoires(Enum):
+    """Enum : EvenementsAleatoires"""
+    AUCUN = 0
+    PARTIEL = 1
+    VENDREDI_MATIN = 2
+    RETOUR_VACANCES = 3
