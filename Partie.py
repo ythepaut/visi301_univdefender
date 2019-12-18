97795416d4763535d9a0eddbe602716573865127
rakotoew <55093676+rakotoew@users.noreply.github.com>
2019-12-18 01:07:13 +0100

    Ajout Carte 2

--- Partie.py
+++ Partie.py
@@ -30,7 +30,8 @@ class Partie():
         self.file_attente_vague = []
         self.dernier_apparition = 0
         self.vie = 10
-        self.argent = 100
+        ##Valeur pour le test de la map, valeur original 100
+        self.argent = 100000000000000 
         self.matiere_courante = Matiere.HISTOIRE
         self.evenement = EvenementsAleatoires.AUCUN
 
